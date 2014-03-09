fac2c29d639222a4d9fa3b4519a449014e686e8b
Chris Hinkle (Siberia) <hinkle@siberia.com>
2014-03-09 14:38:17 -0500

    such structure, much initial

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function( grunt )
+{
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON('package.json'),
+		jade: {
+			compile: {
+				options: { 
+					data: { debug: false, client: false, pretty: true }
+				},
+				files:
+				[ {
+					cwd: "./jade",
+					src: "**/*.jade",
+					dest: "./",
+					expand: true,
+					ext: ".html" 
+				} ]
+			}
+		},
+		sass: {
+			dist: {
+				options: {
+					includePaths: require('node-bourbon').includePaths,
+					outputStyle: 'compressed'
+				},
+				files: {
+					'./css/index.css' : 'scss/index.scss'
+				}
+			}
+		},
+		connect:
+		{
+			server:
+			{
+				options:
+				{
+					port: 8080,
+					base: './',
+					keepalive: false
+				}
+			}
+		},
+		watch: {
+			jade:
+			{
+				files: [ '**/*.jade' ],
+				tasks: [ 'jade' ],
+				options: { livereload:true }
+			},
+		},
+	} );
+
+	grunt.loadNpmTasks( 'grunt-contrib-connect');
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-concat' );
+  grunt.loadNpmTasks( 'grunt-contrib-copy' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-contrib-jade' );
+  grunt.loadNpmTasks( 'grunt-sass' );
+  grunt.registerTask( 'default', [ 'jade', 'sass', 'connect', 'watch'] );
+
+};
