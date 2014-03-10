1de4db76771aaea47427cc649411b55d5c7cae05
Chris Hinkle (Siberia) <hinkle@siberia.com>
2014-03-10 14:50:27 -0500

    such progress

--- js/dogeButton.js
+++ js/dogeButton.js
@@ -1,21 +1,39 @@
 var dogeButton = angular.module( 'dogeButtonApp', [ ] );
-dogeButton.controller( "DogeController", function( $scope, $compile, $log, $rootScope )
+dogeButton.controller( "DogeIndexController", function( $scope, $compile, $log, $rootScope )
 {	
+	$scope.username = 'lilhinx';
 	$scope.createWidgetCode = function( )
 	{
-		var btn = angular.element( document.createElement( 'doge-button' ) );
+		var btn = angular.element( document.createElement( 'doge-embed' ) );
+		btn.setAttribute( 'username', $scope.username );
 	    $scope.widgetCode = $compile( btn )( $scope )[ 0 ];
 	};
 	
-	$rootScope.wow = "wowo";
-	
 	$scope.$watch( "username", function( )
 	{
-		$log.info( "yo" );
-		$rootScope.username = $scope.username;
 		$scope.createWidgetCode( );
 	} );
 	
+} );
+
+dogeButton.controller( "DogeButtonController", function( $scope, $compile, $log, $rootScope )
+{	
+	
+} );
+
+dogeButton.controller( "DogeEmdedController", function( $scope, $compile, $log, $rootScope, $window )
+{
+	$scope.formUrl = function( )
+	{
+		$scope.url = "http://"+ $window.location.host + "/button.html?username="+$scope.username;
+	};
+	
+	$scope.$watch( "username", function( )
+	{
+		$scope.formUrl( );
+	} );
+	
+	$scope.formUrl( );
 } );
 
 dogeButton.directive( "dogeButton", function( )
@@ -31,7 +49,8 @@ dogeButton.directive( "dogeEmbed", function( )
 {
 	return {
 		restrict:'E',
-		scope:{ texts:'=' },
+		scope:{ username:'=' },
+		controller:"DogeEmdedController",
 		templateUrl:'/doge-embed.html'
 	};
 } );
