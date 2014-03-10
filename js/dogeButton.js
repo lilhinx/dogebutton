8d3950840c5e82df7740828ee8bf598da5c9deaa
Chris Hinkle (Siberia) <hinkle@siberia.com>
2014-03-10 12:09:58 -0500

    such progress

--- js/dogeButton.js
+++ js/dogeButton.js
@@ -1,17 +1,20 @@
 var dogeButton = angular.module( 'dogeButtonApp', [ ] );
-dogeButton.controller( "DogeController", function( $scope, $compile, $log )
-{
-	$scope.widgetCode = "wow";
-	
+dogeButton.controller( "DogeController", function( $scope, $compile, $log, $rootScope )
+{	
 	$scope.createWidgetCode = function( )
 	{
-		var btn = angular.element( document.createElement( 'doge-button' ) );      
+		var btn = angular.element( document.createElement( 'doge-button' ) );
 	    $scope.widgetCode = $compile( btn )( $scope )[ 0 ];
-		$log.info( $scope.widgetCode.innerHTML );
-		
 	};
 	
-	$scope.createWidgetCode( );
+	$rootScope.wow = "wowo";
+	
+	$scope.$watch( "username", function( )
+	{
+		$log.info( "yo" );
+		$rootScope.username = $scope.username;
+		$scope.createWidgetCode( );
+	} );
 	
 } );
 
@@ -19,7 +22,16 @@ dogeButton.directive( "dogeButton", function( )
 {
 	return {
 		restrict:'E',
-		scope:{ username:'=', texts:'=' },
-		templateUrl:'/button.html'
+		scope:{ texts:'=' },
+		templateUrl:'/doge-button.html'
+	};
+} );
+
+dogeButton.directive( "dogeEmbed", function( )
+{
+	return {
+		restrict:'E',
+		scope:{ texts:'=' },
+		templateUrl:'/doge-embed.html'
 	};
 } );
