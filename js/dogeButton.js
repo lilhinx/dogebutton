d26f526cedc66d67814d62a7850a208f28f049b9
Chris Hinkle (Siberia) <hinkle@siberia.com>
2014-03-10 16:05:27 -0500

    added at prefix

--- js/dogeButton.js
+++ js/dogeButton.js
@@ -1,7 +1,7 @@
 var dogeButton = angular.module( 'dogeButtonApp', [ ] );
 dogeButton.controller( "DogeIndexController", function( $scope, $compile, $log, $rootScope )
 {	
-	$scope.username = 'lilhinx';
+	$scope.username = '';
 	$scope.createWidgetCode = function( )
 	{
 		var btn = angular.element( document.createElement( 'doge-embed' ) );
@@ -48,7 +48,7 @@ dogeButton.controller( "DogeEmdedController", function( $scope, $compile, $log, 
 {
 	$scope.formUrl = function( )
 	{
-		$scope.url = "http://"+ $window.location.host + "/button.html?username="+$scope.username;
+		$scope.url = "http://"+ $window.location.host + "/button.html?username="+ $scope.username;
 	};
 	
 	$scope.$watch( "username", function( )
@@ -78,3 +78,35 @@ dogeButton.directive( "dogeEmbed", function( )
 		templateUrl:'/doge-embed.html'
 	};
 } );
+
+
+dogeButton.directive( 'atPrefix', function( )
+{
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function( scope, element, attrs, controller )
+		{
+			function hasNoPrefix( value )
+			{
+				return value && !/^\@/.test( value ) && "@".indexOf( value ) === -1;
+			}
+			
+			function ensureAtPrefix( value )
+			{
+				// Need to add prefix if we don't have http:// prefix already AND we don't have part of it
+				if( hasNoPrefix( value ) )
+				{
+					controller.$setViewValue( '@' + value );
+					controller.$render( );
+					return '@' + value;
+				}
+				return value;
+			}
+			
+			controller.$formatters.splice( 0, 0, ensureAtPrefix );
+			controller.$parsers.splice( 0, 0, ensureAtPrefix );
+		}
+	};
+});
+
